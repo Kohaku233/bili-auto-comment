0ddc7d918bcf75ba2a8fa51210665207f7eff1ee
Kohaku233 <2427503528@qq.com>
2023-05-16 00:15:33 +0800

    测试评论接口，视频列表过滤，添加 tag 以及视频描述的正则

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,17 @@
 /* 主程序，负责按时间循环查询某个 Tag 相关的视频 */
+import { commentApi } from "./comment"
 import { searchApi } from "./search"
 const videoList = await searchApi()
-// console.log('videoList', videoList)
-videoList?.filter((video) => {
-  if (
-    video.tag.includes("雫るる") &&
-    video.tag.includes("虚拟主播") &&
+/* 正则表达式，只要出现 雫るる 或者 虚拟主播 这两个 tag 就匹配，但是只要出现雨宮的简体或者繁体就匹配失败 */
+const regexTag = /^(?!.*[\u96E8\u5BAE]).*(雫るる|虚拟主播|vtuber|lulu).*$/
+/* 简介中包含直播间地址或者个人空间地址 */
+const regexDesc = /(387636363|21013446)/
+const filteredVideoList = videoList?.filter((video) => {
+  return (
+    regexTag.test(video.tag) &&
     video.play > 1000 &&
-    video.review > 10
-  ) {
-    console.log(video)
-  }
+    video.review > 10 &&
+    regexDesc.test(video.description)
+  )
 })
+console.log(filteredVideoList)
--- src/request/axiosUtils.ts
+++ src/request/axiosUtils.ts
@@ -4,11 +4,12 @@ import dotenv from "dotenv"
 /* 获取环境变量 */
 dotenv.config({ path: ".env.local" })
 const cookie = process.env.buvid3
+const sessData = process.env.SESSDATA
 
 /* 封装 axios */
 const axiosInstance: AxiosInstance = axios.create({
   headers: {
-    Cookie: `buvid3=${cookie}`
+    Cookie: `buvid3=${cookie}; SESSDATA=${sessData}`
   }
 })
 /* 封装 get 请求 */
@@ -20,11 +21,11 @@ const getRequest = async <T = any>(
   return axiosInstance.get(url, { params, ...config })
 }
 /* 封装 post 请求 */
-const postRequest = async (
+const postRequest = async <T = any>(
   url: string,
   data?: any,
   config?: AxiosRequestConfig
-) => {
+): Promise<AxiosResponse<T>> => {
   return axiosInstance.post(url, data, config)
 }
 
