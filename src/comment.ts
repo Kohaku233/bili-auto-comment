ef696549d18ed0405dcdb57e00a2cd56bbf28c12
Kohaku233 <2427503528@qq.com>
2023-05-19 00:16:08 +0800

    评论接口读取 csrf 配置信息

--- src/comment.ts
+++ src/comment.ts
@@ -1,5 +1,7 @@
 import { postRequest } from "./request/axiosUtils"
-
+import dotenv from "dotenv"
+dotenv.config({ path: ".env.local" })
+const csrf = process.env.bili_jct || ""
 export const commentApi = async (oid: number) => {
   try {
     const url = "https://api.bilibili.com/x/v2/reply/add"
@@ -7,12 +9,14 @@ export const commentApi = async (oid: number) => {
       type: 1,
       oid,
       message: "每个稿件来一句:るるまじ可愛い!!!",
-      csrf: "0d8ab0270b0ddbc60e3c61e13ebb0725"
+      csrf
     }
     const res = await postRequest<ApiResponse<any>>(url, requestBody, {
       headers: { "Content-Type": "application/x-www-form-urlencoded" }
     })
     // console.log(res.data.code)
     return res.data.code
-  } catch (error) {}
+  } catch (error) {
+    console.log('评论请求出现异常',error)
+  }
 }
