ffdbbbb0f45209227b66d88b93485c666d3d96c3
Kohaku233 <2427503528@qq.com>
2023-05-19 00:16:41 +0800

    评论返回成功状态码打印视频链接，列出接下来项目的 TODO 项

--- src/index.ts
+++ src/index.ts
@@ -14,4 +14,16 @@ const filteredVideoList = videoList?.filter((video) => {
     regexDesc.test(video.description)
   )
 })
-console.log(filteredVideoList)
+/* 获取所有满足条件的视频 avid 并存到数组中 */
+const filteredVideoIdList = filteredVideoList?.map((video) => video.aid) ?? []
+filteredVideoIdList.forEach(async (avid) => {
+  const code = await commentApi(avid)
+  /* 如果评论成功，则存入到 json 中以免二次评论 */
+  if (code == 0) {
+    console.log(`http://www.bilibili.com/video/av${avid}`)
+  }
+})
+// TODO: 实现搜索入参自定义化、评论自定义化、筛选自定义化
+// TODO: 实现评论引流，在自己评论下添加子评论并引流到 github 链接
+// TODO: 实现部署上线，自动运行
+// TODO: 实现运行日志详情提示，例如每过一段时间自动汇集信息则B站私信通知
